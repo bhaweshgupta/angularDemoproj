1f8faebb40a8585757ea58065e20065444799b0c
Bhawesh Gupta <talktobhawesh@gmail.com>
2019-03-15 18:04:49 +0530

    angular anchor scroll with database data

---
+++ Demo/Country.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Collections.Generic;
+namespace Demo
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<City> Cities { get; set; }
+    }
+}
---
+++ Demo/Scripts/Script.js
@@ -0,0 +1,15 @@
+﻿/// <reference path="angular.js" />
+var demoApp = angular.module("demoApp", [])
+                     .controller("countryController",
+                        function ($scope, $location, $anchorScroll, $http) {
+                            $http.get("CountryService.asmx/GetData")
+                              .then(function (response) {
+                                  $scope.countries = response.data;
+                              });
+
+                            $scope.scrollTo = function (countryName) {
+                                $location.hash(countryName);
+                                $anchorScroll();
+                            }
+
+                        });
