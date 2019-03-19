2dd29d098b970b45d5faf1d23766c216b32095e5
Bhawesh Gupta <talktobhawesh@gmail.com>
2019-03-19 16:04:51 +0530

    2nd commit after making clear URL
    default routing is not working and page reload is also not working

--- DEmo2/DEmo2/Properties/AssemblyInfo.cs
+++ DEmo2/DEmo2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Demo")]
+[assembly: AssemblyTitle("DEmo2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Demo")]
+[assembly: AssemblyProduct("DEmo2")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("87811f79-1008-4859-b5a7-558780463c31")]
+[assembly: Guid("903ed4e1-0855-446f-a25e-c6385fceb0c3")]
 
 // Version information for an assembly consists of the following four values:
 //
--- DEmo2/DEmo2/Scripts/script.js
+++ DEmo2/DEmo2/Scripts/script.js
@@ -2,7 +2,7 @@
 
 var app = angular
             .module("DEmo2",["ngRoute"])
-            .config(function ($routeProvider) {
+            .config(function ($routeProvider, $locationProvider) {
                 $routeProvider
                     .when("/home", {
                         templateUrl: "Templates/home.html",
@@ -16,16 +16,34 @@ var app = angular
                         templateUrl: "Templates/students.html",
                         controller: "studentsController"
                     })
+                    .when("/students/:id", {
+                        templateUrl: "Templates/studentDetails.html",
+                        controller: "studentDetailsController"
+                    })
+                    .otherwise({
+                        redirectTo: "/home"
+                    })
+                    $locationProvider.html5Mode(true);
             })
             .controller("homeController", function ($scope) {
                 $scope.message = "Home Page";
             })
             .controller("coursesController", function ($scope) {
-                $scope.courses = ["C#", "ANGULAR.JS", "ASP.NET", "SQL Server"];
+                $scope.courses = ["C#", "VB.NET", "ASP.NET", "SQL Server"];
             })
              .controller("studentsController", function ($scope, $http) {
                  $http.get("StudentService.asmx/GetAllStudents")
                                         .then(function (response) {
                                             $scope.students = response.data;
                                         })
-             })
+                                    })
+                                    .controller("studentDetailsController", function ($scope, $http, $routeParams) {
+                                        $http({
+                                            url: "StudentService.asmx/GetStudent",
+                                            method: "get",
+                                            params: { id: $routeParams.id }
+                                        }).then(function (response) {
+                                            $scope.student = response.data;
+                                        })
+
+                                    })
